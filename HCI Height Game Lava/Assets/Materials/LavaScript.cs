3e0eb2524e80dc21730487075f66aa9ae0160bbd
RynRoo <robynderoode@gmail.com>
2020-11-04 12:47:15 +0100

    Added HCI Height Game Lave Map

---
+++ HCI Height Game Lava/Assets/Materials/LavaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class LavaScript : MonoBehaviour {
+ 
+    public float speedX = 0.0000001f;
+    public float speedY = 0.0000001f;
+    private float curX;
+    private float curY;
+ 
+    // Use this for initialization
+    void Start () {
+        curX = GetComponent<Renderer>().material.mainTextureOffset.x;
+        curY = GetComponent<Renderer>().material.mainTextureOffset.y;
+    }
+     
+    // Update is called once per frame
+    void FixedUpdate () {
+        curX += Time.deltaTime * speedX;
+        curY += Time.deltaTime * speedY;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(curX, curY));
+    }
+}
