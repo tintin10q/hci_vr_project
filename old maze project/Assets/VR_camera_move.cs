964069da40b8bac7f9078004ef98d2ee3c49247b
Quinten <tintin10q@hotmail.com>
2020-11-14 11:40:19 +0100

    added my futile attempts

---
+++ old maze project/Assets/VR_camera_move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_camera_move : MonoBehaviour
+{
+	public Transform vrCamera;
+	public float toggleAngle = 30.0f;
+	public float speed = 3.0f;
+	private bool moveForwards = false;
+
+	private CharacterController myCharacterController;
+	public Camera mychamera;
+
+	// Use this for initialization
+	void Start()
+	{
+		myCharacterController = GetComponent<CharacterController>();
+		vrCamera = Camera.main.transform;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (vrCamera.transform.eulerAngles.x >= toggleAngle && vrCamera.transform.eulerAngles.x < 90.0f)
+		{
+			moveForwards = true;
+		}
+		else
+		{
+			moveForwards = false;
+		}
+
+		if (moveForwards)
+		{
+			Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+			myCharacterController.SimpleMove(forward * speed);
+		}
+	}
+}
---
+++ old maze project/Assets/scripts/ballcontroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballcontroll : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody myRigidbody;
+
+	private void Start() {
+		myRigidbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		myRigidbody.AddForce(moveHorizontal*speed, 0.0f, moveVertical*speed);
+	}
+}
