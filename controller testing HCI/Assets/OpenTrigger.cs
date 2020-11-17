639ab48101c7e2ed5be0cfd35f40a87b89e9e29d
QuackWe <r.a.oprea@tilburguniversity.edu>
2020-11-17 18:07:20 +0100

    Made controller stuff and elevator

---
+++ controller testing HCI/Assets/OpenTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenTrigger : MonoBehaviour
+{
+	public Transform OpenDoor;
+	public GameObject Door;
+	public Vector3 moveDirection;
+	
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+		Door.transform.position += moveDirection;
+    }
+	void OnTriggerExit(Collider other)
+	{
+		Door.transform.position -= moveDirection;
+	}
+}
---
+++ controller testing HCI/Assets/TeleportTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTrigger : MonoBehaviour
+{
+	public Transform TeleportTarget;
+	public GameObject Player;
+	
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        Player.transform.position=TeleportTarget.transform.position;
+    }
+}
