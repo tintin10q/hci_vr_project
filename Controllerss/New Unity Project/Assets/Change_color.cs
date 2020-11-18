5af34e0a377ba4dd6ef1784788f09b7f7edc4542
RynRoo <robynderoode@gmail.com>
2020-11-18 23:26:00 +0100

    Changed the visuals

---
+++ Controllerss/New Unity Project/Assets/Change_color.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public void Purple()
+    {
+        GetComponent<Renderer>().material.color = Color.magenta;
+    }
+    
+    public void Blue()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+    }
+    
+    public void Black()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+    
+    
+}
+
