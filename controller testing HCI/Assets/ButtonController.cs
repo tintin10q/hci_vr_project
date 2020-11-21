dc376fb794f6a36916bce7b05ff1dc8e6ba507cd
Quinten <tintin10q@hotmail.com>
2020-11-22 00:11:09 +0100

    Added area and buttons and bridge I got the hang now a bit, Removed old projects

---
+++ controller testing HCI/Assets/ButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonController : MonoBehaviour
+{
+   public Transform targetBridge;
+   public Transform removeBridge1;
+   public Transform removeBridge2;
+
+   private Vector3 showPos = new Vector3(0, 0, 0);
+   private Vector3 awayPos = new Vector3(0, -20, 0);
+   
+   
+   public void ButtonPressCALLTHISONE_ONCLICK()
+   {
+      // removeBridge1.SetActive(false);
+      // removeBridge2.SetActive(false);
+      // targetBridge.SetActive(true);
+      targetBridge.localPosition = showPos;
+      removeBridge1.localPosition = awayPos;
+      removeBridge2.localPosition = awayPos;
+      GetComponent<Renderer>().material.color = Color.red;
+   }
+   
+   private void Update() // set color back to white when bridge is lower
+   {
+      if (targetBridge.localPosition.y < 0) // Set color back to white if button is "unpressed"
+      {
+         GetComponent<Renderer>().material.color = Color.white;
+      }
+   }
+}
---
+++ controller testing HCI/Assets/RailingController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailingController : MonoBehaviour
+{
+    public Transform railingContainer;
+    
+    private Vector3 pos;
+
+    public void onButtonClick_THISONBUTTONCLICK()
+    {
+        if (railingContainer.localPosition.y < -1) // Railing is off
+        {
+            pos = railingContainer.localPosition;
+            pos.y = 0;
+            railingContainer.localPosition = pos;
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+        else // Railing is off
+        {
+            pos = railingContainer.localPosition;
+            pos.y = -20;
+            railingContainer.localPosition = pos;
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
