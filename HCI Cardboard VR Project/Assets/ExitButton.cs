2ef230a12c994f98d513a77782d8a091d9d1eb14
RynRoo <robynderoode@gmail.com>
2020-11-28 13:54:01 +0100

    Added exit button and begin room.

---
+++ HCI Cardboard VR Project/Assets/ExitButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ExitButtonPressCALLTHISONE_ONCLICK()
+    {
+        Debug.Log("Quit game.");
+        Application.Quit();
+    }
+}
